f28d91781503eaa53665d42a630feb9ac40d8aae
luis queral <luismqueral@gmail.com>
2015-06-03 13:34:21 -0400

    ABV comment
    commented out fake ABV values for now

--- script.py
+++ script.py
@@ -12,7 +12,10 @@ for x in range(NUMBER_OF_NAMES):
 	abv1 = file('_assets/abv1.txt').readlines()
 	abv2= file('_assets/abv2.txt').readlines()
 
-	writeme = "%s %s // %s.%s%% ABV" % (random.choice(paint).rstrip(), random.choice(suffix).rstrip(), random.choice(abv1).rstrip(), random.choice(abv2).rstrip() )
+	#include fake ABV content
+	#writeme = "%s %s // %s.%s%% ABV\n" % (random.choice(paint).rstrip(), random.choice(suffix).rstrip(), random.choice(abv1).rstrip(), random.choice(abv2).rstrip() )
+
+	writeme = "%s %s \n" % (random.choice(paint).rstrip(), random.choice(suffix).rstrip(), )
 
 	with open("tweet_" + datetime.now().strftime("%H:%M:%S").replace(':','.') + ".txt",'a') as output:
 	    output.write(writeme)
