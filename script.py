d126f35bc3be7c71406c0094bef6f8fac3ad4bf1
luis queral <luismqueral@gmail.com>
2015-06-03 13:26:56 -0400

    init push

---
+++ script.py
@@ -0,0 +1,20 @@
+#420_NAME_BOT
+
+from datetime import datetime
+import random
+
+NUMBER_OF_NAMES = 1 #change this value for more outputs
+
+for x in range(NUMBER_OF_NAMES):
+
+	paint = file('_assets/paint.txt').readlines()
+	suffix = file('_assets/suffix.txt').readlines()
+	abv1 = file('_assets/abv1.txt').readlines()
+	abv2= file('_assets/abv2.txt').readlines()
+
+	writeme = "%s %s // %s.%s%% ABV" % (random.choice(paint).rstrip(), random.choice(suffix).rstrip(), random.choice(abv1).rstrip(), random.choice(abv2).rstrip() )
+
+	with open("tweet_" + datetime.now().strftime("%H:%M:%S").replace(':','.') + ".txt",'a') as output:
+	    output.write(writeme)
+	
+
